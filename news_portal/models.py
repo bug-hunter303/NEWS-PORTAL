42e85fd99950a7c324c5910e95c18a5b5f58155a
Ankit Khadka <khadka.ankit2004@gmail.com>
2025-07-22 16:41:45 +0545

    FIrst commit

---
+++ news_portal/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+# title , category , ad , suscribe , most popularr
+
+
+# Post :  title , content , feature_image , author , views_count , category , tags , comments
+
