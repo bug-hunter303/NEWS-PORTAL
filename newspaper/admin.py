fccaa6f7bd0148b13ca15e111d4c4e0c2e245b91
ankit <khadka.ankit2004@gmail.com>
2025-10-03 03:23:53 +0545

    Contact page

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Advertisement,Category , Post ,Tag,OurTeam
+from .models import Advertisement,Category , Post ,Tag,OurTeam,Contact
 # Register your models here.
 
 admin.site.register(Post)
@@ -8,3 +8,4 @@ admin.site.register(Tag)
 admin.site.register(Category)
 admin.site.register(Advertisement)
 admin.site.register(OurTeam)
+admin.site.register(Contact)
